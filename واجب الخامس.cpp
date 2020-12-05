18ff8e328a9f8cc49cc6c2871a3474aec96bfec3
saif487 <74253060+saif487@users.noreply.github.com>
2020-12-04 20:04:07 -0800

    Add files via upload

---
+++ واجب الخامس.cpp
@@ -0,0 +1,172 @@
+/* 
+name: seifelnasr eisa adam musa
+class: scond
+deportment: computer
+   */
+/*This program contains both a stack and a queue, and each one of them works alone on the process of adding and deleting */
+#include<iostream.h>
+#include<conio.h>
+#define SIZE 5
+#define MAX 500
+class Queue {
+   private:
+  int items[SIZE], front, rear;
+
+   public:
+  Queue() {
+    front = -1;
+    rear = -1;
+  }
+
+  bool isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isEmpty() {
+    if (front == -1)
+      return true;
+    else
+      return false;
+  }
+
+  void enQueue(int element) {
+    if (isFull()) {
+      cout << "Queue is full";
+    } else {
+      if (front == -1) front = 0;
+      rear++;
+      items[rear] = element;
+      cout << endl
+         << "Inserted " << element << endl;
+    }
+  }
+
+  int deQueue() {
+    int element;
+    if (isEmpty()) {
+      cout << "Queue is empty" << endl;
+      return (-1);
+    } else {
+      element = items[front];
+      if (front >= rear) {
+        front = -1;
+        rear = -1;
+      } /* Q has only one element, so we reset the queue after deleting it. */
+      else {
+        front++;
+      }
+      cout << endl
+         << "Deleted -> " << element << endl;
+      return (element);
+    }
+  }
+
+  void display() {
+    /* Function to display elements of Queue */
+    int i;
+    if (isEmpty()) {
+      cout << endl
+         << "Empty Queue" << endl;
+    } else {
+      cout << endl
+         << "Front index-> " << front;
+      cout << endl
+         << "Items -> ";
+      for (i = front; i <= rear; i++)
+        cout << items[i] << "  ";
+      cout << endl
+         << "Rear index-> " << rear << endl;
+    }
+  }
+};
+class Stack {
+    int top;
+
+public:
+    int a[MAX]; // Maximum size of Stack
+
+    Stack() { top = -1; }
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
+    if (top >= (MAX - 1)) {
+        cout << "Stack Overflow";
+        return false;
+    }
+    else {
+        a[++top] = x;
+        cout << x << " pushed into stack\n";
+        return true;
+    }
+}
+
+int Stack::pop()
+{
+    if (top < 0) {
+        cout << "Stack Underflow";
+        return 0;
+    }
+    else {
+        int x = a[top--];
+        return x;
+    }
+}
+int Stack::peek()
+{
+    if (top < 0) {
+        cout << "Stack is Empty";
+        return 0;
+    }
+    else {
+        int x = a[top];
+        return x;
+    }
+}
+
+bool Stack::isEmpty()
+{
+    return (top < 0);
+}
+
+// Driver program to test above functions
+
+int main() {
+  Queue q;
+
+  //deQueue is not possible on empty queue
+  q.deQueue();
+
+  //enQueue 5 elements
+  q.enQueue(1);
+  q.enQueue(2);
+  q.enQueue(3);
+  q.enQueue(4);
+  q.enQueue(5);
+
+  //6th element can't be added to queue because queue is full
+  q.enQueue(6);
+
+  q.display();
+
+  //deQueue removes element entered first i.e. 1
+  q.deQueue();
+
+  //Now we have just 4 elements
+   cout<<"================================================================"<<endl;
+  q.display();
+   class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+  getch();
+  return 0;
+}
